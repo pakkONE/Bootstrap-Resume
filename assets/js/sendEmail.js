1ba50855b518700fcdfe052f3a452c5ff1a5338d
pakkONE <johan.pakarinen@gmail.com>
2022-05-30 09:01:35 +0000

    Implement EmailJS integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("service_7tx43lm", "template_bdt55x8", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+    return false;
+}
